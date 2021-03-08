49af33eda69a976ec51f64ecaa75caab469e3cd1
Philippe Bourgau <philippe.bourgau@gmail.com>
2021-03-08 16:13:45 +0100

    Working

--- test/Kata.spec.ts
+++ test/Kata.spec.ts
@@ -1,3 +1,18 @@
-describe('Kata', () => {
-  it('should ', () => {});
+describe('Rover', () => {
+  // it('should accept a sequence of commands', () => {
+  //   let rover = new Rover();
+
+  //   rover.forward();
+  //   rover.right();
+  //   rover.forward();
+  //   rover.forward();
+  //   rover.left();
+  //   rover.forward();
+  //   rover.left();
+
+  //   expect(rover.direction()).toBe(Direction.WEST);
+  //   expect(rover.position()).toBe(new Position(2,2));
+  // });
+
+  it('should accept a sequence of commands', () => {});
 });
