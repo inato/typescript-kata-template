5a0ce372a54c3e59c16c33c8c73ebe7a19b4da2b
jrmdayn <dayanjeremie@gmail.com>
2021-03-08 16:39:43 +0100

    Working

--- test/Kata.spec.ts
+++ test/Kata.spec.ts
@@ -30,29 +30,29 @@ describe('Rover', () => {
     expect(rover.direction).toBe(Direction.NORTH);
  });
 
- describe('moving forward', () => {
-    
+   describe('moving forward', () => {
+      
 
-  it('should move forward facing North', () => {
-    const rover = new Rover();
+   it('should move forward facing North', () => {
+      const rover = new Rover();
 
-    rover.forward();
+      rover.forward();
 
-    expect(rover.x).toBe(0);
-    expect(rover.y).toBe(1);
-    expect(rover.direction).toBe(Direction.NORTH);
- });
+      expect(rover.x).toBe(0);
+      expect(rover.y).toBe(1);
+      expect(rover.direction).toBe(Direction.NORTH);
+      });
 
-  it('should move forward facing North', () => {
-    const rover = new Rover();
+      it('should move forward facing North', () => {
+         const rover = new Rover();
 
-    rover.forward();
+         rover.forward();
 
-    expect(rover.x).toBe(0);
-    expect(rover.y).toBe(1);
-    expect(rover.direction).toBe(Direction.NORTH);
- });
-});
+         expect(rover.x).toBe(0);
+         expect(rover.y).toBe(1);
+         expect(rover.direction).toBe(Direction.NORTH);
+      });
+   });
 
   it('should move forward facing North', () => {
     const rover = new Rover();
