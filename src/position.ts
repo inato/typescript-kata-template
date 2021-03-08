4b3c8a37f70966b996f6100085eef5ba19b011d4
jrmdayn <dayanjeremie@gmail.com>
2021-03-08 17:42:49 +0100

    Working

--- src/position.ts
+++ src/position.ts
@@ -1,16 +1,23 @@
-
-export const position=(x, y) =>{
+export const position = (x, y) => {
   return { x, y };
-}
+};
 export interface Position {
   x: number;
   y: number;
 }
 
 export const north = (pos: Position) => {
-    return position(pos.x, pos.y +1)
-}
+  return position(pos.x, pos.y + 1);
+};
 
-export const west =(pos: Position) => {
+export const west = (pos: Position) => {
   return position(pos.x - 1, pos.y);
-}
+};
+
+export const east = (pos: Position) => {
+  return position(pos.x + 1, pos.y);
+};
+
+export const south = (pos: Position) => {
+  return position(pos.x, pos.y - 1);
+};
